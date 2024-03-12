56a85ef387e66c49f42846ec5a24476f37a1dfb2
Sai40k <151876367+Sai40k@users.noreply.github.com>
2024-03-12 19:34:33 +0000

    sai40kapipipi

--- src/app.py
+++ src/app.py
@@ -12,32 +12,30 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
-
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
+@app.route('/members', methods=['GET'])
+def show_all_members():
+    members= jackson_family.get_all_members()
+    return jsonify (members )
+@app.route('/member/<int:member_id>', methods=['GET'])
+def show_one_member(member_id):
+    member= jackson_family.get_member(member_id)
+    return jsonify (member )
+@app.route('/members', methods=['POST'])
+def add_member():
+    new_member= jackson_family.add_member(request.json)
+    return jsonify(new_member)
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def kill_member(member_id):
+    diying_member= jackson_family.delete_member(member_id)
+    return jsonify(diying_member)
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
-@app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
-    return jsonify(response_body), 200
-
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
